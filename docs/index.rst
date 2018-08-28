01f53a0c80395f995c7e6fff18fdf2b47f2f17f9
g.baldi <g.baldi@zerynth.com>
2018-08-28 10:51:09 +0200

    Added docs doc :)
    Task name:
    
    Reason for the commit:
    
    Changes:
    
    * ...

---
+++ docs/index.rst
@@ -0,0 +1,17 @@
+==================
+My Awesome Library
+==================
+
+Here goes the documentation for the library
+
+
+Technical Details
+*****************
+
+Here goes any subsection of the doc
+
+
+This last line must be included! It will parse the actual documentation written in the source
+code; it will also automatically add all the provided examples.
+
+.. include:: __toc.rst
--- library.py
+++ library.py
@@ -3,4 +3,17 @@
 # The name of this file is important!
 # It will be used in the import statement like this:
 #
-# from namespace.libname import thisfile
+#from namespace.libname import thisfile
+
+
+
+
+def my_function(a,b,c):
+    """
+.. function::my_function(a,b,c)
+
+    Here goes the documentation of this function.
+    This will be parsed and included in the final documentation.
+    Remember to add this file to docs.json!
+    """
+    pass
